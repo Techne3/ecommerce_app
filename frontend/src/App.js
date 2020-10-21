97b80b14292b9e135e1594ad9f58aee9f9e98f31
Timothy Sharkey <Techne3@gmail.com>
2020-10-21 17:27:42 -0600

    created add to cart action and reducer -saved cart array in local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <Router>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               amazon
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">Cart</Link>
+            {cartItems.length > 0 && (
+              <span className="badge">{cartItems.length}</span>
+            )}
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
