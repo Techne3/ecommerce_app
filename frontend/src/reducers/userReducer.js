069be63969de9105ff838d4ee44f64b1f25804c7
Timothy Sharkey <Techne3@gmail.com>
2020-11-13 15:28:23 -0700

    new rout to update the user data - Created new form to accept new user data - New Action/reducer funtions to update user info

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -8,6 +8,9 @@ import {
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SUCCESS,
   USER_SIGNOUT,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_RESET,
+  USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants";
 
 export const userRegisterReducer = (state = {}, action) => {
@@ -45,6 +48,21 @@ export const userDetailsReducer = (state = { loading: true }, action) => {
       return { loading: false, user: action.payload };
     case USER_REGISTER_FAIL:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true };
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true };
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, error: action.payload };
+    case USER_UPDATE_PROFILE_RESET:
+      return {};
     default:
       return state;
   }
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,25 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { detailsUser } from "../actions/userActions";
+import { detailsUser, updateUserProfile } from "../actions/userActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
 
 function ProfileScreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
 
   const userDetails = useSelector((state) => state.userDetails);
   const { loading, error, user } = userDetails;
 
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const {
+    success: successUpdate,
+    error: errorUpdate,
+    loading: loadingUpdate,
+  } = userUpdateProfile;
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(detailsUser(userInfo._id));
-  }, [dispatch, userInfo]);
+    if (!user) {
+      dispatch({ type: USER_UPDATE_PROFILE_RESET });
+      dispatch(detailsUser(userInfo._id));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, [dispatch, userInfo, user]);
 
   const submitHandler = (e) => {
     e.preventDefault();
     // dispatch update profile
+    if (password !== confirmPassword) {
+      alert("Password and Confirm Password Do Not Match");
+    } else {
+      dispatch(updateUserProfile({ userId: user._id, name, email, password }));
+    }
   };
 
   return (
@@ -34,14 +58,23 @@ function ProfileScreen() {
           <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <>
+            {loadingUpdate && <LoadingBox></LoadingBox>}
+            {errorUpdate && (
+              <MessageBox variant="danger">{errorUpdate}</MessageBox>
+            )}
+            {successUpdate && (
+              <MessageBox variant="success">
+                Profile Update Successfully
+              </MessageBox>
+            )}
             <div>
               <label htmlFor="name">Name</label>
               <input
                 id="name"
                 type="text"
                 placeholder="Enter Name"
-                value={user.name}
-                onChange={(e) => e.target.value}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div>
@@ -50,7 +83,8 @@ function ProfileScreen() {
                 id="email"
                 type="email"
                 placeholder="Enter Email"
-                value={user.email}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div>
@@ -59,7 +93,7 @@ function ProfileScreen() {
                 id="password"
                 type="password"
                 placeholder="Enter Password"
-                value={user.password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div>
@@ -68,6 +102,7 @@ function ProfileScreen() {
                 id="confirmPassword"
                 type="password"
                 placeholder="Confirm password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
               />
             </div>
             <div>
