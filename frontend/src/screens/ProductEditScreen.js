57ce859c684f6aa38040f993b050258b62dc5dc3
Timothy Sharkey <Techne3@gmail.com>
2020-11-17 11:47:03 -0700

    Created Route in BE to update product info - New Action/ reducer to send new AJAX request to updateProduct route

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { detailsProduct } from "../actions/productActions";
+import { detailsProduct, updateProduct } from "../actions/productActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
 
 function ProductEditScreen(props) {
   const productId = props.match.params.id;
@@ -10,37 +11,67 @@ function ProductEditScreen(props) {
   const [price, setPrice] = useState("");
   const [image, setImage] = useState("");
   const [category, setCategory] = useState("");
-  const [brand, setBrand] = useState("");
   const [countInStock, setCountInStock] = useState("");
+  const [brand, setBrand] = useState("");
   const [description, setDescription] = useState("");
 
-  const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
-  const { product, error, loading } = productDetails;
+  const { loading, error, product } = productDetails;
 
+  const productUpdate = useSelector((state) => state.productUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate;
+
+  const dispatch = useDispatch();
   useEffect(() => {
-    if (!product) {
+    if (successUpdate) {
+      props.history.push("/productlist");
+    }
+    if (
+      !product ||
+      product._id !== productId ||
+      successUpdate ||
+      successUpdate
+    ) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
       dispatch(detailsProduct(productId));
     } else {
       setName(product.name);
       setPrice(product.price);
       setImage(product.image);
       setCategory(product.category);
-      setBrand(product.brand);
       setCountInStock(product.countInStock);
+      setBrand(product.brand);
       setDescription(product.description);
     }
-  }, [dispatch, product, productId]);
-
+  }, [product, dispatch, productId, successUpdate, props.history]);
   const submitHandler = (e) => {
     e.preventDefault();
+    // TODO: dispatch update product
+    dispatch(
+      updateProduct({
+        _id: productId,
+        name,
+        price,
+        image,
+        category,
+        brand,
+        countInStock,
+        description,
+      })
+    );
   };
   return (
     <div>
-      <from className="form" onSubmit={submitHandler}>
+      <form className="form" onSubmit={submitHandler}>
         <div>
-          <h1>EditProduct {productId}</h1>
+          <h1>Edit Product {productId}</h1>
         </div>
+        {loadingUpdate && <LoadingBox></LoadingBox>}
+        {errorUpdate && <MessageBox variant="danger">{errorUpdate}</MessageBox>}
         {loading ? (
           <LoadingBox></LoadingBox>
         ) : error ? (
@@ -52,70 +83,71 @@ function ProductEditScreen(props) {
               <input
                 id="name"
                 type="text"
-                placeholder="Enter Name"
+                placeholder="Enter name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-              />
+              ></input>
             </div>
             <div>
               <label htmlFor="price">Price</label>
               <input
                 id="price"
                 type="text"
-                placeholder="Enter Price"
+                placeholder="Enter price"
                 value={price}
                 onChange={(e) => setPrice(e.target.value)}
-              />
+              ></input>
             </div>
             <div>
-              <label htmlFor="image">image</label>
+              <label htmlFor="image">Image</label>
               <input
                 id="image"
                 type="text"
-                placeholder="Enter Image"
+                placeholder="Enter image"
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
-              />
+              ></input>
             </div>
             <div>
-              <label htmlFor="category">category</label>
+              <label htmlFor="category">Category</label>
               <input
                 id="category"
                 type="text"
-                placeholder="Enter Category"
+                placeholder="Enter category"
                 value={category}
                 onChange={(e) => setCategory(e.target.value)}
-              />
+              ></input>
             </div>
             <div>
-              <label htmlFor="brand">brand</label>
+              <label htmlFor="brand">Brand</label>
               <input
                 id="brand"
                 type="text"
-                placeholder="Enter Brand"
+                placeholder="Enter brand"
                 value={brand}
                 onChange={(e) => setBrand(e.target.value)}
-              />
+              ></input>
             </div>
             <div>
               <label htmlFor="countInStock">Count In Stock</label>
               <input
                 id="countInStock"
                 type="text"
-                placeholder="Enter Count In Stock"
+                placeholder="Enter countInStock"
                 value={countInStock}
                 onChange={(e) => setCountInStock(e.target.value)}
-              />
+              ></input>
             </div>
             <div>
               <label htmlFor="description">Description</label>
-              <input
+              <textarea
                 id="description"
+                rows="3"
                 type="text"
-                placeholder="Enter Description"
+                placeholder="Enter description"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
-              />
+              ></textarea>
             </div>
             <div>
               <label></label>
@@ -125,7 +157,7 @@ function ProductEditScreen(props) {
             </div>
           </>
         )}
-      </from>
+      </form>
     </div>
   );
 }
